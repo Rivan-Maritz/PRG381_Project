06d0c94fa360f70a7d803a4dd7598faf0dfbc91f
Rivan-Maritz <601530@student.belgiumcampus.ac.za>
2026-07-10 15:33:29 +0200

    Migrate project from IntelliJ to NetBeans. My fault

---
+++ src/main/java/com/mycompany/prg381_project/PRG381_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prg381_project;
+
+/**
+ *
+ * @author rivan
+ */
+public class PRG381_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
