3115c488c137894fde4d22ba7c29ac455a539396
Rivan Maritz <601530@student.belgiumcampus.ac.za>
2026-07-12 18:13:53 +0200

    Completed Project structure.
    Presentation Layer  jframes to be added by frontend team

--- src/main/java/com/mycompany/prg381_project/DataLayer/cleanersDAO.java
+++ src/main/java/com/mycompany/prg381_project/DataLayer/cleanersDAO.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.prg381_project.DataLayer.model;
+package com.mycompany.prg381_project.DataLayer;
 
 /**
  *
  * @author rivan
  */
-public class usersModel {
+public class cleanersDAO {
     
 }
--- src/main/java/com/mycompany/prg381_project/model/stockissuanceModel.java
+++ src/main/java/com/mycompany/prg381_project/model/stockissuanceModel.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.prg381_project.DataLayer.model;
+package com.mycompany.prg381_project.model;
 
 /**
  *
  * @author rivan
  */
-public class suppliersModel {
+public class stockissuanceModel {
     
 }
