4eb0c8dfc26c30c4a87d68f8662b269dd44535f5
Rivan Maritz <601530@student.belgiumcampus.ac.za>
2026-07-12 17:56:44 +0200

    Added DataLayer structure + Connection String

---
+++ src/main/java/com/mycompany/prg381_project/DataLayer/DBConnection.java
@@ -0,0 +1,15 @@
+package com.mycompany.prg381_project.DataLayer;
+
+  import java.sql.Connection;
+  import java.sql.DriverManager;
+  import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/cleaning_inventory_db";
+    private static final String User = "postgre";
+    private static final String Password = "jamesRamero_54@34";
+    
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, User, Password);
+    }
+}
